2cf0942eed359ea4f3ea40610bea16f0382c0b5a
Sivert Berg <sivertb@stud.ntnu.no>
2011-12-18 19:00:33 +0100

    Added GPL license file and headers

--- src/mmsget.c
+++ src/mmsget.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2011 Sivert Berg <sivertb@stud.ntnu.no>
+ *
+ * This file is part of mmsget - a threaded mms-stream downloader
+ *
+ * mmsget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mmsget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mmsget.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
